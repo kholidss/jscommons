bc4a42bf8b3c4b9003f53d089d12a3d9256b8a49
Ryan Smith <0ryansmith1994@gmail.com>
2017-12-03 17:05:07 +0000

    fix(overwriteModel): Fixes overwrite in memoryRepo.

--- src/memoryRepo/models/overwrite.ts
+++ src/memoryRepo/models/overwrite.ts
@@ -1,9 +1,29 @@
+import { first } from 'lodash';
+import NoModel from '../../errors/NoModel';
 import { Opts, Result } from '../../repoFactory/models/OverwriteSignature';
+import Model from '../../utils/Model';
 import Config from './Config';
-import patch from './patch';
 
-export default <R>(config: Config<R>) => {
+export default <R>({ getModels, setModels, modelName }: Config<R>) => {
   return async ({ id, record }: Opts<R>): Promise<Result<R>> => {
-    return patch(config)({ id, patch: record });
+    const prevModels = getModels();
+    const predicate = (model: Model<R>) => model.id === id;
+    const matchedModels = prevModels.filter(predicate);
+    const overwrittenModels = matchedModels.map(() => {
+      return {
+        ...(record as any as object),
+        id,
+      } as Model<R>;
+    });
+    const overwrittenModel = first(overwrittenModels);
+
+    if (overwrittenModel === undefined) {
+      throw new NoModel(modelName);
+    }
+
+    const unmatchedModels = prevModels.filter((model) => !predicate(model));
+    const nextModels = [...unmatchedModels, ...overwrittenModels];
+    setModels(nextModels);
+    return { model: overwrittenModel };
   };
 };
